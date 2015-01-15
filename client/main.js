327c10d888c91b74bc812fa9a23db5c8444515b4
Kai Vermehr <k@eboy.com>
2015-01-15 02:30:24 -0800

    Switched to saving binaries to MongoDB, Date
    + deleted CollectionFS code
    + added remove button
    + added createdAt field
    + inverse sorting by date
    + small CSS improvements

--- client/main.js
+++ client/main.js
@@ -1,60 +1,39 @@
-Template.imagesShow.helpers({
-	'showImages': function() {
-		// return Images.find();
-		return Images.find({}, {sort: {uploadedAt: -1}});
-	}
-})
-
-Template.imagesShow.events({
-	'click .remove': function() {
-		console.log("Removing \"" + this._id + "\"");
-		Images.remove(this._id);
-	}
-})
-
-
-
-Template.imagesCount.helpers({
-	'posts': function() {
-		return Images.find().count();
-	}
-});
-
-Template.imageUpload.events({
-	'change .myFileInput': function(event, template) {
-		FS.Utility.eachFile(event, function(file) {
-			Images.insert(file, function (err, fileObj) {
-				//Inserted new doc with ID fileObj._id, and kicked off the data upload using HTTP
-				console.log("Inserting \"" + fileObj._id + "\"");
-			})
-		})
-	}
-});
-
 Template.pixUpload.events({
 	'change .myPixInput': function(event, template) {
 		event.preventDefault();
 		var file = event.target.files[0];
 		var reader = new FileReader();
 
-		// reader.onload = function(event){
-		// 	var result = reader.result;
-		// 	var buffer = new Uint8Array(result);
-		// 	MyPix.insert({binary: buffer});
-		// }
-		// reader.readAsArrayBuffer(file);
-
 		reader.onload = function(event){
-			MyPix.insert({binary: reader.result});
+			MyPix.insert({
+				binary: reader.result,
+				createdAt: new Date
+			});
 		}
-
 		reader.readAsDataURL(file);
-	}
-});
-
-Template.pixList.helpers({
-	'showPix': function(){
-		return MyPix.find();
 	}
 })
 
+Template.pixCount.helpers({
+	'posts': function() {
+		return MyPix.find().count();
+	}
+})
+
+Template.pixList.helpers({
+	'showPix': function(){
+		return MyPix.find({}, {sort: {createdAt: -1}});
+	},
+	'thedate': function(){
+		var date = this.createdAt;
+		var formattedDate = moment(date).format('hh:mm:ss/dddd/DD/MMMM/YYYY');
+		return "date: " + formattedDate;
+	}
+})
+
+Template.pixList.events({
+	'click .remove': function() {
+		console.log("Removing \"" + this._id + "\"");
+		MyPix.remove(this._id);
+	}
+})
--- server/publish.js
+++ server/publish.js
@@ -1,9 +1,5 @@
 //Publish methods
 
-Meteor.publish('Images', function() {
-	return Images.find();
-});
-
 Meteor.publish('MyPix', function() {
 	return MyPix.find();
 })
