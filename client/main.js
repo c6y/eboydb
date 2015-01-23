8ed0b567eb3083ee1dfb3a32f47e987160092682
Kai Vermehr <k@eboy.com>
2015-01-23 03:15:43 -0800

    Add copyright, now has 2 File stores
    – 2 stores for images: ScaledPix and OriginalPix
    – pixList Template takes images from ScaledPix
    – line of code creates metadata field and adds copyright

--- client/main.js
+++ client/main.js
@@ -1,14 +1,29 @@
 Template.pixUpload.events({
 	'change .myPixInput': function(event, template) {
-		event.preventDefault();
-		var file = event.target.files[0];
-		if (!file) return;
-		MyPix.insert(file, function (err, fileObj) {
-			//Inserted new doc with ID fileObj._id,
-			//and kicked off the data upload using HTTP
+		FS.Utility.eachFile(event, function(file) {
+	
+			var newFile = new FS.File(file);
+			newFile.metadata = {copyright: "eBoy"};
+
+			MyPix.insert(newFile, function(err, fileObj) {
+				//If !err, we have inserted new doc with ID fileObj._id, and
+				//kicked off the data upload using HTTP
+			});
 		});
 	}
-})
+});
+
+// Template.pixUpload.events({
+// 	'change .myPixInput': function(event, template) {
+// 		event.preventDefault();
+// 		var file = event.target.files[0];
+// 		if (!file) return;
+// 		MyPix.insert(file, function (err, fileObj) {
+// 			//Inserted new doc with ID fileObj._id,
+// 			//and kicked off the data upload using HTTP
+// 		});
+// 	}
+// })
 
 Template.pixCount.helpers({
 	'posts': function() {
