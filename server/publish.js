b0492f92270c3c2b3270f4444dc3f87c3bec206a
Kai Vermehr <k@eboy.com>
2015-01-28 02:53:25 -0800

    Single views of documents now do permalink

--- server/publish.js
+++ server/publish.js
@@ -4,3 +4,14 @@ Meteor.publish('MyPix', function(cursor) {
 	Counts.publish(this, 'numberOfPosts', MyPix.find(), { noReady: true });
 	return MyPix.find({}, {sort: {uploadedAt: -1}, limit: 4, skip: cursor});
 });
+
+// Meteor.publish('aPix', function(id) {
+// 	check(id, String);
+// 	return MyPix.find(_id);
+// });
+
+Meteor.publish('aPix', function(id) {
+  check(id, String);
+  console.log('id: ' + id);
+  return MyPix.find(id);
+});
