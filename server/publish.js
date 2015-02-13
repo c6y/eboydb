1c2d192e31d3f80d8201e2f6a530add564e8bd36
Kai Vermehr <k@eboy.com>
2015-02-13 04:00:36 -0800

    Improved search
    - searches tags and names
    - empty searches are routed to all

--- server/publish.js
+++ server/publish.js
@@ -13,5 +13,23 @@ Meteor.publish('PixPage', function(slug, page) {
 	} else {
 		return MyPix.find({"metadata.tags" : slug}, {sort: {uploadedAt: -1}, limit: displayQty, skip: cursor});
 	}
-	
 });
+
+// Meteor.publish('PixQuery', function(slug, page) {
+// 	var reg = RegExp(slug, 'i', 's')
+// 	return MyPix.find({
+// 		"metadata.tags" : {
+// 			$regex: reg
+// 		}
+// 	})
+// })
+
+Meteor.publish('PixQuery', function(slug, page) {
+	var reg = RegExp(slug, 'i', 's')
+	return MyPix.find({
+		$or: [
+			{"metadata.tags" : {$regex: reg}},
+			{"original.name" : {$regex: reg}}
+		]
+	})
+})
